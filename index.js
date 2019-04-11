5973e14b56d28e9c44b486148e66582f88d0e7e5
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2019-04-11 20:54:13 +0200

    Modify index

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const nodeLoader = require('./src/loaders/nodeLoader')
 const classicInstancer = require('./src/instancers/classic')
 const builder = require('./src/builder')
+const numas = require('./src/core/numas')
+
 
 const loader = nodeLoader.loadSync('./node_modules/numas.wasm/numas.wasm')
-classicInstancer.instantiate(loader).then(builder.buildModule)
-
-
+classicInstancer.instantiate(loader).catch(e => console.error(e))
