14d9d130f10ab7e734a7e34a82b71cfd621f5504
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2019-04-11 18:59:24 +0200

    Add NArray class

---
+++ src/core/array.js
@@ -0,0 +1,62 @@
+const datatypes = require('../metadata/datatypes').DATATYPES
+const prototypes = require('../metadata/prototypes')
+
+
+class NArray {
+    /**
+     * Initialized new numas array
+     * 
+     * @param {number} pointer Pointer into linear memory
+     * @param {string} type Type string representation
+     * @returns {void}
+     */
+    constructor(pointer, type) {
+        if (!datatypes.hasOwnProperty(type)) {
+            throw 'Invalid datatype provided'
+        }
+
+        this.pointer = pointer
+        this.type = type
+        this.prototype = prototypes.getPrototype(type)
+    }
+
+    /**
+     * Creates new array with given data and shape
+     * 
+     * @param {Array} data Data in array
+     * @param {Array} shape Shape of array
+     * @returns {NArray}
+     */
+    static new(data, shape) {
+        
+    }
+
+    /**
+     * Returns length of array
+     * 
+     * @returns {number}
+     */
+    len() {
+        return this.prototype.len(this.pointer)
+    }
+
+    /**
+     * Returns cloned array
+     * 
+     * @returns {NArray}
+     */
+    clone() {
+        return new NArray(this.prototype.clone(this.pointer), this.type)
+    }
+    
+    /**
+     * Returns view into array
+     * 
+     * @returns {NArray}
+     */
+    view() {
+        return new NArray(this.prototype.view(this.pointer), this.type)
+    }
+}
+
+module.exports = NArray
