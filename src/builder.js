7ded53e796aa44b6901ee9f61f35b7d5178ac20a
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2019-04-11 18:58:29 +0200

    Refactor builder

--- src/builder.js
+++ src/builder.js
@@ -1,11 +1,33 @@
-let func = null
-let mod = null
+const callbacks = []
 
+/**
+ * Add callback that is called when numas module is instantiated
+ * 
+ * @param {Function} callback Callback
+ * @returns {void}
+ */
+const addCallback = callback => {
+    callbacks.push(callback)
+}
+
+/**
+ * Invokes callbacks with wasmModule as argument
+ * 
+ * @param {Object} wasmModule WebAssembly numas module
+ * @returns {void}
+ */
 const buildModule = wasmModule => {
-    mod = wasmModule
-    func = wasmModule.instance.exports
+    const mod = {
+        memory: wasmModule.instance.exports.memory,
+        functions: wasmModule.instance.exports,
+    }
+
+    for (callback of callbacks) {
+        callback(mod)
+    }
 } 
 
 module.exports = {
     buildModule,
+    addCallback,
 }
