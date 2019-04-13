94be470bef51f5ba01abeb8d2c5b271f7b617285
Lukáš Zahradník <lukaszahradnik96@seznam.cz>
2019-04-14 00:57:31 +0200

    Modify building

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const classicInstancer = require('./src/instancers/classic')
 const builder = require('./src/builder')
 const numas = require('./src/core/numas')
 
+const defaultNode = {
+    instancer: classicInstancer,
+    loader: nodeLoader.loadAsync,
+    path: './node_modules/numas.wasm/numas.wasm'
+}
 
-const loader = nodeLoader.loadSync('./node_modules/numas.wasm/numas.wasm')
-classicInstancer.instantiate(loader).catch(e => console.error(e))
+const init = (callback, config = {}) => {
+    const { instancer, loader, path } = { ...defaultNode, ...config }
+    
+    builder.addCallback(callback)
+    instancer.instantiate(loader(path)).then(builder.buildModule)
+}
+
+module.exports = {
+    numas,
+    init,
+    config: {
+        defaultNode,
+    },
+}
